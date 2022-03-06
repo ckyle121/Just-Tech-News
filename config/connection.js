2ae8725cd8483232519cbc7f88f5ae11b37a7f61
ckyle121 <carolinekyle21@gmail.com>
2022-03-06 12:52:49 -0600

    using bcrypt

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// import Sequelize constructor from the library 
+const Sequelize = require('sequelize');
+
+// import dotenv for password security 
+require('dotenv').config();
+
+// create connection to our databse, pass in MySql info for username/password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
---
+++ models/User.js
@@ -0,0 +1,73 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration 
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS
+        // define id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        // define password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIGURATION OPTIONS 
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }  
+        },
+        // pass in our imported sequelize connection (direct connection to database)
+        sequelize,
+
+        // don't automatically create timestamp fields
+        timestamps: false,
+
+        // don't pluralize name of database table
+        freezeTableName: true,
+
+        // use underscores instead of camel-casing
+        underscored: true, 
+
+        // make it so our model name stays lowercase in database
+        modelName: 'user'
+    }
+);
+
+module.exports = User; 
