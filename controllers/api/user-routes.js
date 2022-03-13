fe36c0d8599072bc892484751d7d4e514e8c5e69
ckyle121 <carolinekyle21@gmail.com>
2022-03-13 11:03:48 -0500

    add login and logout with sessions functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,14 +63,22 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
 });
 
-// POST /login 
+// LOGIN
 router.post('/login', (req, res) => {
     // QUERY OPERATION 
     User.findOne({
@@ -87,11 +95,31 @@ router.post('/login', (req, res) => {
 
         if (!validPassword){
             res.status(400).json({ message: 'Incorrect Password'});
+            return;
         }
-        
-        res.json({ user: dbUserData, message: 'You are now logged in'});
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in'});
+        });
     });
 });
+
+// LOGOUT
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn){
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+})
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
