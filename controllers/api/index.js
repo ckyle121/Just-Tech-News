9bd21d3eb7ba51c4840644e59eef519b743d5954
ckyle121 <carolinekyle21@gmail.com>
2022-03-14 14:13:54 -0500

    final changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
@@ -8,4 +8,4 @@ router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
-module.exports = router; 
+module.exports = router;
